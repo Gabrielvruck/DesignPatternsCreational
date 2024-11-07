716a968e99e20839bc88b42d3d7cf0c0d5623ca6
Gabriel Vruck <gabrielvruck@gmail.com>
2024-11-07 12:20:05 -0300

    coreCRM

---
+++ DesignPatternsCreational/Infrastructure/Integrations/CoreCrmIntegrationService.cs
@@ -0,0 +1,12 @@
+﻿using DesignPatternsCreational.Application.Models;
+
+namespace DesignPatternsCreational.Infrastructure.Integrations
+{
+    public class CoreCrmIntegrationService : ICoreCrmIntegrationService
+    {
+        public void Sync(OrderInputModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
